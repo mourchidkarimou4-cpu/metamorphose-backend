fd73d521030b8beee67acdfe0e6fccd3362ccf29
Mourchid KARIMOU <mourchidkarimou4@gmail.com>
2026-04-02 09:19:38 +0100

    fix: media servis en production + suppression doublon paiement

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from administration import views as administration_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,5 +15,6 @@ urlpatterns = [
     path('api/cadeaux/', include('cadeaux.urls')),
     path('api/avis/', include('avis.urls')),
     path('api/paiement/', include('paiement.urls')),
-    path('api/paiement/', include('paiement.urls')),
+    # Media — fonctionne en dev ET en production
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
